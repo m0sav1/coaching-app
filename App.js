990c68b1664459b94a50ece0f204122c5532d0fa
Sebastian S <92436020+Lobos91@users.noreply.github.com>
2023-04-13 17:12:48 +0200

    tweaking configurering, auth cfg added

--- App.js
+++ App.js
@@ -1,24 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
-import { getFirestore, setDoc, doc } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+import { Firestore_Db } from "./firebaseConfig";
 
-// Just test to check conecction
-const sendDataToFireBase = async () => {
-  const firestore = getFirestore();
+export default function App() {
+  // JUST FOR TEST
+  const sendDataToFireBase = async () => {
+    await setDoc(doc(Firestore_Db, "users", "user_id2"), {
+      email: "test2@gmail.com",
+      login: "IamAwesome",
+    });
+  };
 
-  await setDoc(doc(firestore, "users", "user_id"), {
-    email: "test2@gmail.com",
-    login: "IamAwesome",
-  });
-};
-
-return (
-  <View style={styles.container}>
-    <Text>Welcome to coaching app!</Text>
-    <Button title="Send data (Test)" onPress={sendDataToFireBase} />
-    <StatusBar style="auto" />
-  </View>
-);
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to coaching app!</Text>
+      <Button title="Send data (Test)" onPress={sendDataToFireBase} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBM3qMuVDxSFJS0bCrU7Fo8s-rpMJs-FLo",
+  authDomain: "phantoms-mobile-app.firebaseapp.com",
+  projectId: "phantoms-mobile-app",
+  storageBucket: "phantoms-mobile-app.appspot.com",
+  messagingSenderId: "733350156577",
+  appId: "1:733350156577:web:62c601aa9fcb9ec8ae4660",
+  measurementId: "G-6V593ZNWE3",
+};
+
+export const Firebase_App = initializeApp(firebaseConfig);
+export const Firestore_Db = getFirestore(Firebase_App);
+export const Firebase_Auth = getAuth(Firebase_App);
