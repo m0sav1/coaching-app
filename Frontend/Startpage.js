7a6cfb0d73939ecedbf4292663df7a78d5c4b707
Isabella <90841191+isabellajonssson@users.noreply.github.com>
2023-04-13 19:06:07 +0200

    .

--- Frontend/Startpage.js
+++ Frontend/Startpage.js
@@ -8,7 +8,7 @@ import React from 'react';
  */
 
 const Startpage = () => {
-  const videoUrl = require('./phantoms-movie.mp4');
+  const videoUrl = require('./assets/phantoms-movie.mp4');
   const videoRef = React.useRef(null);
   const { width, height } = Dimensions.get('window');
 
