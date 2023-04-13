3feac99d92df67c80877394aec2721bd85bcd627
Sebastian S <92436020+Lobos91@users.noreply.github.com>
2023-04-13 11:03:27 +0200

    databas uppkopling

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { initializeApp } from "firebase/app";
+import { getFirestore, setDoc, doc } from "firebase/firestore";
 
 export default function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBM3qMuVDxSFJS0bCrU7Fo8s-rpMJs-FLo",
+    authDomain: "phantoms-mobile-app.firebaseapp.com",
+    projectId: "phantoms-mobile-app",
+    storageBucket: "phantoms-mobile-app.appspot.com",
+    messagingSenderId: "733350156577",
+    appId: "1:733350156577:web:62c601aa9fcb9ec8ae4660",
+    measurementId: "G-6V593ZNWE3",
+  };
+
+  initializeApp(firebaseConfig);
+
+  // Just test to check conecction
+  const sendDataToFireBase = async () => {
+    const firestore = getFirestore();
+
+    await setDoc(doc(firestore, "users", "user_id"), {
+      email: "sebaswat@gmail.com",
+      login: "testlogin",
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text>Welcome to coaching app!</Text>
+      <Button title="Send data (Test)" onPress={sendDataToFireBase} />
       <StatusBar style="auto" />
     </View>
   );
