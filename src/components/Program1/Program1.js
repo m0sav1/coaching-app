55f7852e1adc4d3d79ce5c620e38cc6e79351955
Sebastian S <92436020+Lobos91@users.noreply.github.com>
2023-05-03 12:28:29 +0200

    Files fetch init

--- src/components/Program1/Program1.js
+++ src/components/Program1/Program1.js
@@ -1,10 +1,57 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import {getStorage, ref, listAll} from "firebase/storage"
+import { Video } from "expo-av";
+import firebaseConfig from "../../../firebaseConfig";
+
+
 
 const Program1 = () => {
+  const [videos, setVideos] = useState([]);
+
+
+  
+   useEffect(() => {
+    const storage = getStorage();
+    const listRef = ref(storage, "PersonligUtveckling/");
+
+    listAll(listRef)
+      .then((res) => {
+        
+        const promises = res.items.map((itemRef) =>
+          itemRef.getDownloadURL().then((url) => ({
+            name: itemRef.name,
+            // url,
+          })),
+          console.log(res.items[0].name),
+          // console.log(res.items[0].url)
+
+        );
+        Promise.all(promises).then((videos) => {
+          setVideos(videos);
+      
+          
+        })
+       
+      })
+      .catch((error) => {
+        
+        console.log(error);
+      });
+  }, []);
+  
+
+
   return (
     <View>
-      <Text style={styles.container}> Program 1</Text>
+     <Text style={styles.container}>Program 1</Text>
+      {videos.map((video) => (
+        <View key={video.name}>
+          <Text>{video.name}</Text>
+          {/* <Video source={{ uri: video.url }} /> */}
+        </View>
+      ))}
     </View>
   );
 };
