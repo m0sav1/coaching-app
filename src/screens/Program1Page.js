b253a7dd536e2e52f99ac2517073de27b5dd9c2e
m0sav1 <arefmosavi786@gmail.com>
2023-05-05 09:16:43 +0200

    some tiny changes

--- src/screens/Program1Page.js
+++ src/screens/Program1Page.js
@@ -41,16 +41,17 @@ const Program1Page = () => {
 
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text style={{ fontSize: 20 }}>Text in the middle, here wil come some info about the corse
-         that you chose to learn and I can give credit already now you made a great decision</Text>
+         that you chose to learn. I can give you credit already, you made a great decision by choosing 
+         our application.------------------Marhaba bekom ana takallom belArabi showay whoway, Habibi come to DUBAI.</Text>
       </View>
 
-      <View style={{ paddingBottom: 20 }}>
+      <View style={{ paddingBottom: 30 }}>
         <TouchableOpacity
           style={{
             backgroundColor: "blue",
-            padding: 10,
-            borderRadius: 5,
-            marginHorizontal: 20,
+            padding: 20,
+            borderRadius: 20,
+            marginHorizontal: 30,
           }}
           onPress={() => navigation.navigate("HomePage")}
         >
@@ -62,3 +63,5 @@ const Program1Page = () => {
 };
 
 export default Program1Page;
+
+
